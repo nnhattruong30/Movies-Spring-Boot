459b68d768ffb887bee593cbdb667b4fbf1e6676
Truong Nguyen <nnhattruong30@gmail.com>
2023-04-29 01:43:49 +0700

    applied styles to Hero component

--- client/src/components/hero/Hero.js
+++ client/src/components/hero/Hero.js
@@ -11,7 +11,7 @@ const Hero = ({ movies }) => {
                     return (
                         <Paper key={idx}>
                             <div className="movie-card-container">
-                                <div className='movie-card'>
+                                <div className='movie-card' style={{"--img": `url(${movie.backdrops[0]})`}}>
                                     <div className="movie-detail">
                                         <div className="movie-poster">
                                             <img src={movie.poster} alt="" />
